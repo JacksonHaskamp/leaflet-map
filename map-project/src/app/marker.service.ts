c1b68903e508519398ecbd3c977eb9c71be6a121
Jackson Haskamp <jacksonhaskamp@gmail.com>
2023-05-04 21:50:40 -0500

    got backend and frontend on web servers
    https://jacksonhaskamp.github.io/leaflet-map/map
    
    https://marker-backend1.onrender.com
    
    https://dashboard.render.com/web/srv-cha6qkak728r8859kq10/deploys/dep-cha6qkqk728r8859kqrg

--- map-project/src/app/marker.service.ts
+++ map-project/src/app/marker.service.ts
@@ -12,7 +12,7 @@ export class MarkerService {
   
 
   createMarker(marker: any) {
-    const apiUrl = 'http://localhost:3000/markers';
+    const apiUrl = 'https://marker-backend1.onrender.com/markers';
     return this.http.post(apiUrl, marker).pipe(
       tap((newMarker: any) => {
         // Emit the created marker
@@ -22,19 +22,19 @@ export class MarkerService {
   }
 
   getMarkers() {
-    const apiUrl = 'http://localhost:3000/markers'; 
+    const apiUrl = 'https://marker-backend1.onrender.com/markers'; 
     return this.http.get(apiUrl);
   }
 
   updateMarker(marker: any) {
     const id = marker._id;
     console.log(id);
-    return this.http.put('http://localhost:3000/markers/' + id, marker);
+    return this.http.put('https://marker-backend1.onrender.com/markers/' + id, marker);
   }
   
   deleteMarker(id: string) {
     console.log(id);
-    return this.http.delete('http://localhost:3000/markers/' + id);
+    return this.http.delete('https://marker-backend1.onrender.com/markers/' + id);
   }
   
   
